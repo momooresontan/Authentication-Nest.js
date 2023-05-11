58e2d103703d29b1134e93981783ac3866ee9077
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-11 09:49:20 +0100

    local strategy implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UsersService],
-  providers: [AuthService],
+  imports: [UsersService, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {
+    super(); //config
+  }
+
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(username, password);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
 }
