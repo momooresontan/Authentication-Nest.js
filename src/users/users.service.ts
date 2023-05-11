dd9c4114dc089c07ec2f2ddd80aeabe769ac8095
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-11 08:54:02 +0100

    First commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
+@Injectable()
+export class UsersService {}
