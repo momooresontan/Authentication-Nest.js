8c23b832abbdd035897c69683a519e5f7e108eb5
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-11 09:22:14 +0100

    local strategy created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,5 +7,12 @@ export class AuthService {
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(username);
+
+    if (user && user.password === password) {
+      const { password, username, ...rest } = user;
+      return rest;
+    }
+
+    return null;
   }
 }
