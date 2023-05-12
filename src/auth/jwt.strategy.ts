9a06bff311e3e1a7954c2a0fce29ff4f26c1bea6
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-12 10:14:23 +0100

    jwt strategy implemented and jwt auth guard created

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,14 +1,23 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
+      jwtFromReqest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: 'SECRET', // use .env variables
     });
+  }
+
+  async validate(payload: any) {
+    //const user = await this.usersService.getById(payload.sub)
+    return {
+      id: payload.sub,
+      name: payload.name,
+    };
   }
 }
