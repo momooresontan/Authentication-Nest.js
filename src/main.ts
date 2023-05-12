bf067844e832f8e3db10bbedef7c225eee6790ad
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-12 02:45:16 +0100

    session created

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 360000 },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
