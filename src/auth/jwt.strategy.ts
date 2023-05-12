b43c7af826d649f5784b036f671f1b1d99837683
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-12 10:03:25 +0100

    jwt strategy created

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      ignoreExpiration: false,
+      secretOrKey: 'SECRET', // use .env variables
+    });
+  }
+}
