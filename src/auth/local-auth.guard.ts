c6aa3ed2349a577fa96a66804e09ebd33c3d7b9b
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-12 07:58:14 +0100

    canActivate function added to local auth guard

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -1,9 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
-  async canActivate(context: ExecutionContext) {}
+  async canActivate(context: ExecutionContext) {
+    const result = (await super.canActivate(context)) as boolean;
+    const request = context.switchToHttp().getRequest();
+
+    await super.logIn(request);
+    return result;
+  }
 }
